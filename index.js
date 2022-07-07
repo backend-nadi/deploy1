4e7c88389c52ceb6c07c566cd46994066d4cbb72
rolandadifan <rolandadifandana@gmail.com>
2022-07-07 09:36:02 +0700

    add data

--- index.js
+++ index.js
@@ -8,4 +8,49 @@ app.get('/', (req,res) => {
     res.send("hellow")
 })
 
+app.get('/api/users', (req,res) => {
+    const data = [
+        {
+            nama: 'bambino',
+            umur: 11,
+            hobi:['cat', 'memukul', 'memaki']
+        },
+        {
+            nama: 'jacky',
+            umur: 10,
+            hobi:['nonton', 'eeq', 'kang pukul']
+        },
+        {
+            nama: 'diana',
+            umur: 19,
+            hobi:['hah', 'babi', 'memaki']
+        },
+        {
+            nama: 'kambing',
+            umur: 18,
+            hobi:['es', 'api', 'udara']
+        },
+        {
+            nama: 'maria',
+            umur: 19,
+            hobi:['he', 'ho', 'pa']
+        },
+        {
+            nama: 'facu',
+            umur: 7,
+            hobi:['cat', 'test', 'babi']
+        },
+        {
+            nama: 'janaro',
+            umur: 16,
+            hobi:['eeq', 'memukul', 'memaki']
+        },
+    ]
+
+    res.status(200).json({
+        status: 'success',
+        data: data
+    })
+})
+
 app.listen(port, () => console.log(`lister port ${port}`))
